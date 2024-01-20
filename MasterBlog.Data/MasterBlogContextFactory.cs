6d658db21f7ed31c37c7371d461dfc7d515b0cea
unknown <ducdo080@gmail.com>
2024-01-21 01:37:46 +0700

    Frist Commit

---
+++ MasterBlog.Data/MasterBlogContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterBlog.Data
+{
+    internal class MasterBlogContextFactory:IDesignTimeDbContextFactory<MasterBlogContext>
+    {
+        public MasterBlogContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<MasterBlogContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new MasterBlogContext(builder.Options);
+
+
+        }
+    }
+}
